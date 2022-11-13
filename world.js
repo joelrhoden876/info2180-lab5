790f58ccce6e80ecda3b56fe1ede2c984338fe4e
Joel Rhoden <joelrhodenjm@gmail.com>
2022-11-13 18:07:42 -0500

    Edited world.js

--- world.js
+++ world.js
@@ -1,6 +1,7 @@
 window.onload=function(){
 
     let countryLookupButton;
+    let cityLookupButton;
     let httpRequest;
     let phpResponse;
     let searchfield;
@@ -9,23 +10,27 @@ window.onload=function(){
     searchfield = document.getElementById("country");
     phpResponse = document.getElementById("result");
     countryLookupButton= document.getElementById("lookupco");
-
+    cityLookupButton = document.getElementById("lookupci");
 
     countryLookupButton.addEventListener("click", function(){
         httpRequest = new XMLHttpRequest();
         httpRequest.onreadystatechange = showResponse;
-        httpRequest.open("GET", "world.php?country=" + searchfield.value);
+        httpRequest.open("GET", "world.php?country=" + searchfield.value );
         httpRequest.send();
     })
 
+    cityLookupButton.addEventListener("click", function(){
+        httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = showResponse;
+        httpRequest.open("GET", "world.php?city=" + searchfield.value );
+        httpRequest.send();
+    })
     
     function showResponse(){
         if (httpRequest.readyState === XMLHttpRequest.DONE){
             if (httpRequest.status === 200){
-                // alert(httpRequest.responseText);
                 phpResponse.textContent = httpRequest.responseText;
                 phpResponse.innerHTML = phpResponse.textContent;
-                console.log(httpRequest.responseText);
             }
             else{
                 alert("Invalid request.");
