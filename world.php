8996a2981513c375ff6fe4a85b8c6a70bb8327c9
Joel Rhoden <joelrhodenjm@gmail.com>
2022-11-13 18:07:24 -0500

    Edited world.php

--- world.php
+++ world.php
@@ -11,48 +11,42 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $country = $_REQUEST["country"];
-// echo "<table>
-// <style>
-// table,th,td{
-//   border:1px solid black;
-//   border-collapse: collapse;
-// }
-// </style>
-// <tr>
-// <th>Firstname</th>
-// <th>Lastname</th>
-// <th>Age</th>
-// </tr>
-// <td>.$country.</td>
-// <td></td>
-// <td></td>
-// </table>";
+$city = $_REQUEST["city"]; 
 
-
-
-
-if ($country != ""){
-  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  showTable($results);
-  // foreach ($results as $row){
-  //   // showTable($results);
-  //   //echo "<li>".$row['name'] . ' is ruled by ' . $row['head_of_state']."</li>"; 
-  //   //echo "<table>"."<tr>"."<th>"."Firstname"."</th>"."</tr>"."</table>";
-  // } 
-}
-if ($country == ""){
-  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-  $stmt = $conn->query("SELECT * FROM countries");
-  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  showTable($results);
-  // foreach ($results as $row){
-  //   echo "<li>".$row['name'] . ' is ruled by ' . $row['head_of_state']."</li>"; 
-  // } 
+if (isset($country)){
+  if ($country != ""){
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    showCountryTable($results);
+  }
+  if ($country == ""){
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    showCountryTable($results);
+  }
 }
 
-function showTable($results){
+if (isset($city)){
+  if ($city != ""){
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $stmt = $conn->query("SELECT c.district, c.name as city, c.country_code, cs.name as country, c.population FROM cities c join countries cs on c.country_code = cs.code WHERE cs.name LIKE '%$city%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    showCityTable($results);
+  }
+
+  else if ($city == ""){
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    showCountryTable($results);
+  }
+}
+
+
+
+function showCountryTable($results){
   echo "<table><style>table,th{width:100%;height:100%;border:1px solid black;border-collapse: collapse;table-layout:fixed;margin-left:auto;margin-right:auto;}td{border:1px solid black;padding:10px;}</style><tr>
   <th>Name</th>
   <th>Continent</th>
@@ -60,9 +54,19 @@ function showTable($results){
   <th>Head of State</th>
   </tr>";
   foreach ($results as $row){
-    //echo "<li>".$row['name'] . ' is ruled by ' . $row['head_of_state']."</li>"; 
     echo "<tr>"."<td>".$row['name']."</td>"."<td>".$row['continent']."</td>"."<td>".$row['independence_year']."</td>"."<td>".$row['head_of_state']."</td>"."</tr>";
   
+  }
+}
+
+function showCityTable($results){
+  echo "<table><style>table,th{width:100%;height:100%;border:1px solid black;border-collapse: collapse;table-layout:fixed;margin-left:auto;margin-right:auto;}td{border:1px solid black;padding:10px;}</style><tr>
+  <th>Name</th>
+  <th>District</th>
+  <th>Population</th>
+  </tr>";
+  foreach ($results as $row){
+    echo "<tr>"."<td>".$row['city']."</td>"."<td>".$row['district']."</td>"."<td>".$row['population']."</td>"."<td>"."</tr>";
   }
 }
 
